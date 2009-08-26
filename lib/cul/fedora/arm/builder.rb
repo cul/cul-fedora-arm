33ae17b6bae588f0998a8cdb0e380c6fb4fd8493
James Stuart <tastyhat@jamesstuart.org>
2009-08-26 17:11:54 -0400

    added more comments for rdoc

--- lib/cul/fedora/arm/builder.rb
+++ lib/cul/fedora/arm/builder.rb
@@ -1,20 +1,44 @@
+
+
 module Cul
   module Fedora
     module Arm
+
+      # This class is for building ARM resource models in Fedora
+      #
+      # Authors::  James Stuart (mailto:james.stuart at columbia.edu), Benjamin Armintor (mailto: ba2213 at columbia.edu)
+      # License::
+      # Dependencies:: activesupport 
+      
       class Builder
+
+        # TODO: abstract this to allow for multiple patterns
+        
+        # list of columns for a template with no header row
         DEFAULT_TEMPLATE_HEADER = [:sequence, :aggregate_under, :metadata, :metadata_type, :content, :content_type, :id, :license]
         
+        # columns that must have values
         REQUIRED_COLUMNS = [:sequence]
+        
+        # columns that must be included in a template
         MANDATORY_COLUMNS = [:sequence, :aggregate_under, :metadata]
+        
+        # list of columns which may have values
         VALID_COLUMNS = [:sequence, :aggregate_under, :metadata, :metadata_type, :content, :content_type, :id, :license]
 
+
+        # array of individual hash: each hash corresponds to a metadata or resource.
         attr_reader :parts
         
+
+        # creates a Builder object. Can be used with no arguments, or with ONE of the following options
+        # [:template]:: builds parts based on an enumerable list of strings (for example, the result of File.open) 
         def initialize(*args)
           options = args.extract_options!
 
           @parts = []
 
+          # TODO: add file option to avoid :template => File.open(file_name,"r")
           
           if template = options.delete(:template)
             parse_template(template)
@@ -23,7 +47,8 @@ module Cul
           raise ArgumentError, "arguments should only include one of the following: :template" unless options.empty?
         end
 
-
+        # adds one part to the parts array
+        # arguments are a hash array of column name (underscored symbols) to values
         def add_part(*args)
           value_hash = args.extract_options!
           
@@ -34,29 +59,41 @@ module Cul
           
           @parts << value_hash
         end
-        
+
+        # looks for a part by :sequence key
+        # note: if loading from a template, sequence will not be an integer, but rather a string
         def part_by_sequence(sequence_id)
           @parts.detect { |p| p[:sequence] == sequence_id}
         end
         
         protected
         
+
+        # checks keys of a hash to make sure all elements of REQUIRED_COLUMNS are contained. 
         def test_for_required_columns(value_hash)
           missing_values = REQUIRED_COLUMNS.select { |col| !value_hash.has_key?(col) || value_hash[col].nil? }
           raise "Missing required values #{missing_values.join(",")}" unless missing_values.empty?
         end
         
+        # checks list of column names to make sure every one is in VALID_COLUMNS 
         def test_for_invalid_columns(columns)
           invalid_columns = columns.select { |c| !VALID_COLUMNS.include?(c) }
           raise "Invalid column(s) found: #{invalid_columns.join(",")}" unless invalid_columns.empty?          
         end
         
+        # parses an enumerable of strings to build parts
+        # an optional header row specifies the columns, however sequence must be included and must be first
         def parse_template(template)
-          header_columns = []
-          custom_header = false
+          header_columns = []     # list of columns
+          custom_header = false   # indicates whether a header_row was used
+          
+          
           template.each_with_index do |line, i|
-            # check for header
+          
+            # if first row, check for header
             if i == 0
+          
+              # assumes that if the first character is a letter, this is a header row, otherwise, use default_columns
               case line.to_s
               when /^[0-9]/
                 header_columns = DEFAULT_TEMPLATE_HEADER
@@ -67,19 +104,23 @@ module Cul
               end
               
 
+              # check to make sure all mandatory columns are in the template
               missing_mandatory_columns = MANDATORY_COLUMNS.select { |c| !header_columns.include?(c) }
               raise "Missing mandatory column(s) found: #{missing_mandatory_columns.join(",")}" unless missing_mandatory_columns.empty?
 
               test_for_invalid_columns(header_columns)
 
+              # skip header_row
               next if custom_header
 
             end
+            
+            # create hash out of columns and whitespace-stripped values
             value_hash = Hash[*header_columns.zip(line.split("\t").collect(&:strip)).flatten]
             add_part(value_hash)
           end
           
-          header_columns
+          parts
         end
         
         
