0c800dc22c6b2cc7e727067c39b932cdc0dac461
Benjamin Armintor <armintor@gmail.com>
2015-10-09 17:46:49 -0400

    fallback to Rails 3 gem name

--- lib/cul-fedora-arm.rb
+++ lib/cul-fedora-arm.rb
@@ -1,6 +1,11 @@
+# coding: utf-8
 require "cul/fedora/arm/foxml_builder"
 require "cul/fedora/arm/builder"
 require "cul/fedora/arm/tasks"
 require "cul/fedora/connector"
-require "activesupport"
+begin
+	require "activesupport"
+rescue LoadError
+	require "active_support"
+end
 require "ruby-fedora"
